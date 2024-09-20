d4a6d159e3b3439ecb0ed5f3288097dd81ab29c3
Kovalovszky Patrik <kovapatrik@gmail.com>
2024-09-20 18:23:02 +0200

    feat: add initial implementation

--- src/vacuum.rs
+++ src/vacuum.rs
@@ -1,1 +1,171 @@
+use std::fs;
+use zed_extension_api::{self as zed, settings::LspSettings, LanguageServerId, Result};
 
+struct VacuumBinary {
+    path: String,
+    args: Option<Vec<String>>,
+}
+
+struct VacuumExtension {
+    cached_binary_path: Option<String>,
+}
+
+impl VacuumExtension {
+    fn language_server_binary(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<VacuumBinary> {
+        let binary_settings = LspSettings::for_worktree("vacuum", worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.binary);
+        let binary_args = binary_settings
+            .as_ref()
+            .and_then(|binary_settings| binary_settings.arguments.clone());
+
+        if let Some(path) = binary_settings.and_then(|binary_settings| binary_settings.path) {
+            return Ok(VacuumBinary {
+                path,
+                args: binary_args,
+            });
+        }
+
+        if let Some(path) = worktree.which("vacuum") {
+            return Ok(VacuumBinary {
+                path,
+                args: binary_args,
+            });
+        }
+
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(VacuumBinary {
+                    path: path.clone(),
+                    args: binary_args,
+                });
+            }
+        }
+        zed::set_language_server_installation_status(
+            language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = zed::latest_github_release(
+            "daveshanley/vacuum",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let (platform, arch) = zed::current_platform();
+
+        let asset_name = format!(
+            "vacuum_{version}_{os}_{arch}.tar.gz",
+            // Release versions always starts with a `v`, but the binaries contains only the version number
+            version = release
+                .version
+                .strip_prefix("v")
+                .unwrap_or(&release.version),
+            os = match platform {
+                zed::Os::Mac => "darwin",
+                zed::Os::Linux => "linux",
+                zed::Os::Windows => "windows",
+            },
+            arch = match arch {
+                zed::Architecture::Aarch64 => "arm64",
+                zed::Architecture::X86 => "i386",
+                zed::Architecture::X8664 => "x86_64",
+            }
+        );
+        let remote_release_binary = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no matching asset found: {:?}", asset_name))?;
+
+        let version_dir = format!("vacuum-{}", release.version);
+
+        let binary_name = match platform {
+            zed::Os::Windows => "vacuum.exe",
+            _ => "vacuum",
+        };
+        let binary_path = format!("{version_dir}/{binary_name}");
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+            zed::download_file(
+                &remote_release_binary.download_url,
+                &version_dir,
+                zed::DownloadedFileType::GzipTar,
+            )
+            .map_err(|e| format!("failed to download file: {e}"))?;
+
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(VacuumBinary {
+            path: binary_path,
+            args: binary_args,
+        })
+    }
+}
+
+impl zed::Extension for VacuumExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let vacuum_binary = self.language_server_binary(language_server_id, worktree)?;
+        Ok(zed::Command {
+            command: vacuum_binary.path,
+            args: vacuum_binary
+                .args
+                .unwrap_or_else(|| vec!["language-server".into()]),
+            env: Default::default(),
+        })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+}
+
+zed::register_extension!(VacuumExtension);
